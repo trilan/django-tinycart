ee3e859eb14491221a7d16a62efb417f6853532c
Denis Veselov <veselov@trilan.ru>
2012-03-26 17:16:24 +0400

    move delete_objects function into handle method

--- tinycart/management/commands/delete_lost_cartitems.py
+++ tinycart/management/commands/delete_lost_cartitems.py
@@ -5,21 +5,18 @@ from django.core.management.base import BaseCommand
 from tinycart.models import CartItem
 
 
-def delete_objects(verbosity=1):
-    stdout = sys.stdout
-    count_lost_items = 0
-    for item in CartItem.objects.all():
-        if item.product is None:
-            item.delete()
-            count_lost_items += 1
-    if verbosity > 0:
-        stdout.write('Delete %d cart items\n' % count_lost_items)
-
-
 class Command(BaseCommand):
 
     help = 'Remove lost (deleted) products from cart items.'
 
     def handle(self, *args, **options):
-        self.verbosity = int(options.get('verbosity', 1))
-        delete_objects(verbosity=self.verbosity)
+        verbosity = int(options.get('verbosity'))
+        count_lost_items = 0
+
+        for item in CartItem.objects.all():
+            if item.product is None:
+                item.delete()
+                count_lost_items += 1
+
+        if verbosity > 0:
+            sys.stdout.write('Delete %d cart items\n' % count_lost_items)
